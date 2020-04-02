6f17d43f66d714062f64263e6bfd608d873abb95
Konrad Koc <konradkoc1@gmail.com>
2020-04-02 14:13:22 +0200

    Initial commit

---
+++ src/components/menu-item/Menu-item.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './menu-item.styles.css'
+
+const MenuItem = ( { title, imageUrl, size } ) => (
+
+    <div className={`${size} menu-item`} >
+        <div className='background-image' style={{backgroundImage: `url(${imageUrl})`}}/>
+        <div className='content'>
+            <h2 className='title'>{title.toUpperCase()}</h2>
+            <span className ='subtitle'>SHOP NOW</span>
+        </div>
+    </div>
+)
+
+export default MenuItem
---
+++ src/pages/homepage/Homepage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './homepage.styles.css'
+import Directory from '../../components/directory/Directory'
+
+const Homepage = () => (
+    
+    <div className='homepage'>
+        <Directory />
+    </div>
+)
+
+export default Homepage
