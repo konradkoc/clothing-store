2ea2c91eefe6ed407d842b428111a78fa8c94046
Konrad Koc <konradkoc1@gmail.com>
2020-04-03 10:50:59 +0200

    Added Header component with a little bit of navigation between pages

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
 import React from 'react';
 import './App.css';
+
 import Homepage from './pages/homepage/Homepage';
 import ShopPage from './pages/shoppage/ShopPage'
-import {Route} from 'react-router-dom'
+import Header from './components/header/Header'
+
+import {Route, Switch} from 'react-router-dom'
 
 
 function App() {
   return (
     <div>
-      <Route exact path='/' component={Homepage} />
-      <Route exact path='/shop' component ={ShopPage}/>
+
+      <Header />
+      <Switch>
+        <Route exact path='/' component={Homepage} />
+        <Route exact path='/shop' component ={ShopPage}/>
+      </Switch>
+
     </div>
   );
 }
