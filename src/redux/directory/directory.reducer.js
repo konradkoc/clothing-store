04cdc0c8959874c79badc5c4bbfcfe24f5ee295a
Konrad Koc <konradkoc1@gmail.com>
2020-04-20 11:43:50 +0200

    changed homepage for gird layout, changed few styles

--- src/redux/directory/directory.reducer.js
+++ src/redux/directory/directory.reducer.js
@@ -4,7 +4,7 @@ const INITIAL_STATE = {
         
     {
         title: 'women',
-        imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
+        imageUrl:' https://images.unsplash.com/photo-1492633423870-43d1cd2775eb?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80',
         size: 'large',
         id: 1,
         linkUrl: 'shop/women'
@@ -12,7 +12,7 @@ const INITIAL_STATE = {
 
     {
         title: 'men',
-        imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
+        imageUrl: 'https://images.unsplash.com/photo-1564564321837-a57b7070ac4f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=600&q=60',
         size: 'large',
         id: 2,
         linkUrl: 'shop/men'
@@ -20,21 +20,21 @@ const INITIAL_STATE = {
 
     {    
         title: 'hats',
-        imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
+        imageUrl: 'https://images.unsplash.com/photo-1533055640609-24b498dfd74c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=967&q=80',
         id: 3,
         linkUrl: 'shop/hats'
     },
 
     {
         title: 'jackets',
-        imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
+        imageUrl: 'https://images.unsplash.com/photo-1551488831-00ddcb6c6bd3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=600&q=60',
         id: 4,
         linkUrl: 'shop/jackets'
     },
 
     {
         title: 'sneakers',
-        imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
+        imageUrl: 'https://images.unsplash.com/photo-1516383286093-f90be8db5d45?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=675&q=80',
         id: 5,
         linkUrl: 'shop/sneakers'
     },
