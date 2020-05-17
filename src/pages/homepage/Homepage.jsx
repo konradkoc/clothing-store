fb2040e6cb8a4de58cbff156e61eb68be52d5114
Konrad Koc <konradkoc1@gmail.com>
2020-05-17 14:44:40 +0200

    test worked, reverse change

--- src/pages/homepage/Homepage.jsx
+++ src/pages/homepage/Homepage.jsx
@@ -4,10 +4,8 @@ import Directory from '../../components/directory/Directory'
 const Homepage = () => (
     
     <div className='homepage'>
-        <h1>hello</h1>
         <Directory />
     </div>
 )
 
 export default Homepage
-
